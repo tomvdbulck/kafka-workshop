bab3864a7e5811c00140303ee999e34fefab6b4d
Tom Van den Bulck <tom.vandenbulck@ordina.be>
2021-04-13 21:02:28 +0200

    solution 3

--- receiver/src/main/java/be/ordina/workshop/streaming/TrafficEventReceiver.java
+++ receiver/src/main/java/be/ordina/workshop/streaming/TrafficEventReceiver.java
@@ -1,22 +1,16 @@
 package be.ordina.workshop.streaming;
 
 import be.ordina.workshop.streaming.domain.TrafficEvent;
-import be.ordina.workshop.streaming.domain.VehicleClass;
-import org.apache.kafka.common.serialization.Serdes;
-import org.apache.kafka.streams.kstream.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.annotation.Input;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Sink;
-import org.springframework.kafka.support.serializer.JsonSerde;
 import org.springframework.stereotype.Component;
 
 @Component
 //lab3 and lab4: add something over here
-@EnableBinding({ Sink.class, KStreamSink.class })
+@EnableBinding({ Sink.class})
 public class TrafficEventReceiver {
 
 	private static final Logger logger =
@@ -29,27 +23,16 @@ public class TrafficEventReceiver {
 	}
 
 	//lab 4: Native Kafka Stream processing
-	@StreamListener
-	public void consumeEvent(@Input(KStreamSink.INPUT) KStream<String, TrafficEvent> stream) {
+	public void consumeEvent() {
 		//lab 4:
+		// - take in a stream
+		// - only process VehicleClass.CAR
 		// - determine key
 		// - group on this key: think about serialization
 		// - use aggregate function with helper class: Average.java
 		// - map
 		// - print output
 
-		stream.filter(((key, trafficEvent) -> VehicleClass.CAR == trafficEvent.getVehicleClass()))
-				.selectKey((key, value) -> value.getSensorId())
-				.groupByKey(Grouped.with(Serdes.String(), new JsonSerde<>(TrafficEvent.class)))
-				.windowedBy(TimeWindows.of(120_000L))
-				.aggregate(Average::new, (sensorId, trafficEvent, average) -> {
-					average.addSpeed(trafficEvent.getTrafficIntensity(),
-							trafficEvent.getVehicleSpeedCalculated());
-					return average;
-				}, Materialized.with(Serdes.String(), new JsonSerde<>(Average.class)))
-				.mapValues(Average::average)
-				.toStream()
-				.print(Printed.toSysOut());
 	}
 
 }
